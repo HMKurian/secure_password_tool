2dc3c70894c37fc5e5be389fa9ead9b6ef4964d8
HMKurian <HANNAH.KURIAN48@QMAIL.CUNY.EDU>
2025-07-24 15:02:53 -0400

    local hw done

---
+++ main.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res, next) => {
+    res.json({message: "Hello, World!"});
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
