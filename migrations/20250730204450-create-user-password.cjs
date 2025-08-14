472b098098486f3267780e50898af9eb9693f751
HMKurian <HANNAH.KURIAN48@QMAIL.CUNY.EDU>
2025-08-14 14:56:02 -0400

    updated userpasswords migration

--- migrations/20250730204450-create-user-password.cjs
+++ migrations/20250730204450-create-user-password.cjs
@@ -10,19 +10,54 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       ownerUserId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       url: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       username: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       password: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       sharedByUserId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      label: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      weak_encryption: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      source_password_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: 'UserPasswords',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
       },
       createdAt: {
         allowNull: false,
