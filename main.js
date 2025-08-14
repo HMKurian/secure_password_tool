36838f6fce3b548e6e40a6faabb996df3a107e1a
HMKurian <HANNAH.KURIAN48@QMAIL.CUNY.EDU>
2025-08-14 14:30:16 -0400

    updated with save api

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ import * as bodyParser from "body-parser";
 import bcrypt from 'bcryptjs';
 import * as JWT from 'jsonwebtoken';
 
+import {expressjwt} from "express-jwt";
+import crypto from "crypto";
+
 app.get('/', (req, res, next) => {
     res.json({message: "Hello, World!"});
     // Add this temporarily to your main.js
@@ -20,6 +23,13 @@ app.listen(port, () => {
 });
 
 app.use(bodyParser.default.json());
+
+app.use(
+    expressjwt({
+        secret: process.env.JWT_SECRET,
+        algorithms: ["HS256"],
+    }).unless({ path: ["/login", "/signup", '/'] })
+);
 
 //sign up
 app.post('/signup', async (req, res, next) => {
@@ -72,6 +82,35 @@ app.post('/login', async (req, res, next) => {
     }
 });
 
+app.post('/passwords/save', async (req, res, next) => {
+    const { url, username, password, encryption_key, label } = req.body;
+    const userId = req.auth.id;
+    const modelsObj = await models.default;
+    const userRecord = await modelsObj.User.findOne({
+        attributes: ['encryption_key'], where: { id: userId }
+    });
+    if (!userRecord) {
+        res.status(403);
+        return res.json({message: 'Unable to find the account'});
+    }
+    const matched = await bcrypt.compare(encryption_key, userRecord.encryption_key);
+    if (!matched) {
+        res.status(400);
+        return res.json({message: 'Incorrect encryption key'});
+    }
+    if (!(username && password && url)) {
+        res.status(400);
+        return res.json({message: 'Missing parameters'});
+    }
+    const encryptedUsername = encrypt(username, encryption_key);
+    const encryptedPassword = encrypt(password, encryption_key);
+    const result = await modelsObj.UserPassword.create({
+        ownerUserId: userId, password: encryptedPassword, username: encryptedUsername, url, label
+    });
+    // users_passwords id, owner_user_id, url, username, password, shared_by_user_id, created_at, updated_at
+    res.status(200);
+    res.json({message: 'Password is saved'});
+});
 
 async function hashStr(str) {
     const salt = await bcrypt.genSalt(10);
@@ -88,4 +127,24 @@ function generateJWT(user) {
                 resolve(token);
             });
     });
+}
+
+
+function encrypt(unenrypted_string, key) {
+    const algorithm = 'aes-256-ctr';
+    const iv = crypto.randomBytes(16);
+    const encKey = crypto.createHash('sha256').update(String(key)).digest('base64').slice(0, 32)
+    const cipher = crypto.createCipheriv(algorithm, encKey, iv);
+    let crypted = cipher.update(unenrypted_string,'utf-8',"base64") + cipher.final("base64");
+    return `${crypted}-${iv.toString('base64')}`;
+}
+
+function decrypt(encStr, key) {
+    const algorithm = 'aes-256-ctr';
+    const encArr = encStr.split('-');
+    const encKey = crypto.createHash('sha256').update(String(key)).digest('base64').slice(0, 32);
+    const decipher = crypto.createDecipheriv(algorithm, encKey, Buffer.from(encArr[1], 'base64'));
+    let decrypted = decipher.update(encArr[0], 'base64', 'utf-8');
+    decrypted += decipher.final('utf-8');
+    return decrypted;
 }
